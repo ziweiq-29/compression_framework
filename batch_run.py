3047da9ca16df4c6c190956d26ca78e844e9f464
Ziwei Qiu <ziweiqdeyouxiang@outlook.com>
2025-05-19 06:40:15 -0400

    blockwise with batch run added

--- batch_run.py
+++ batch_run.py
@@ -16,6 +16,13 @@ parser.add_argument("--mode", default="REL", help="Compression mode (ABS, REL, e
 parser.add_argument("--results_dir", default="results", help="Directory to save result CSVs")
 parser.add_argument("--enable-qcat", action="store_true", help="Enable qcat evaluation")
 parser.add_argument("--datatype", choices=["f", "d"], required=True, help="Data type (f for float, d for double)")
+
+parser.add_argument("--enable-calc_stats", action="store_true", help="Enable calcErrStats.py evaluation")
+parser.add_argument("--block-size", type=int, default=-1, help="Block size for calcErrStats.py (-1 for global)")
+parser.add_argument("--shift-size", type=int, help="Shift size for blockwise calcErrStats.py (optional)")
+parser.add_argument("--output-prefix", type=str, help="Output prefix for calcErrStats.py, required if block-size > 0")
+
+
 parser.add_argument("--qcat-evaluators", type=str, default="ssim,compareData",
                     help="Comma-separated list of qcat evaluators to use (default: 'ssim,compareData')")
 parser.add_argument(
@@ -69,6 +76,19 @@ for fname in files:
             continue  # Skip this file if datatype is missing
     if args.qcat_evaluators:
         cmd.extend(["--qcat-evaluators", args.qcat_evaluators])
+    
+    
+    if args.enable_calc_stats:
+        file_results_dir = os.path.join(args.results_dir, os.path.splitext(fname)[0])
+        output_prefix = os.path.join(file_results_dir, os.path.splitext(fname)[0])
+        os.makedirs(file_results_dir, exist_ok=True)
+        cmd.append("--enable-calc_stats")
+        cmd.extend([
+            "--block-size", str(args.block_size),
+            "--shift-size", str(args.shift_size),
+            "--output-prefix", output_prefix
+        ])
+
     print("cmd: ",cmd)
     
     print(f"🔹 Running on {fname}...")
